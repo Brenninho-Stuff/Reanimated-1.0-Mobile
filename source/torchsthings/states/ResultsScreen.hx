1edda757093ea1e15a66cb5acfb587b8e4f91e63
TorchTheDragon <1thatdragonboi@gmail.com>
2025-03-08 11:41:41 -0500

    Fix Results screen

--- source/torchsthings/states/ResultsScreen.hx
+++ source/torchsthings/states/ResultsScreen.hx
@@ -19,6 +19,7 @@ import torchsthings.objects.results.ClearPercentCounter;
 import torchsthings.objects.results.ResultsScore;
 import torchsthings.objects.results.TallyCounter;
 import torchsthings.shaders.LeftMaskShader;
+import flixel.util.FlxSignal;
 
 import states.FreeplayState;
 import states.StoryMenuState;
@@ -580,7 +581,10 @@ class ResultsScreen extends MusicBeatState {
                 obj.anim.addBySymbol('intro', data.intro_name, 24, false);
                 obj.anim.addBySymbol('idle', data.loop_name, 24, true);
                 obj.anim.play('intro');
-                obj.anim.onComplete = function() {obj.anim.play('idle', true);}
+                var signal = new FlxSignal();
+                signal.add(function() {obj.anim.play('idle', true);});
+                obj.anim.onComplete = signal;
+                //obj.anim.onComplete = function() {obj.anim.play('idle', true);}
                 obj.anim.pause();
             case 'sparrowatlas':
                 obj = new FlxSprite();
