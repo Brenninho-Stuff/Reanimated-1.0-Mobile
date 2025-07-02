9def63defdcd358f0060453d89785354f6390314
JorgeX_YT <152746897+ZariuX@users.noreply.github.com>
2025-07-01 18:53:19 -0600

    Deaths Customs

--- source/substates/GameOverSubstate.hx
+++ source/substates/GameOverSubstate.hx
@@ -79,16 +79,28 @@ class GameOverSubstate extends MusicBeatSubstate
 		Conductor.songPosition = 0;
 
 		var playBF:String = PlayState.instance.boyfriend.curCharacter;
+
+		var customCharPath = Paths.getPath('characters/deads/$playBF-dead.json', TEXT, null, true);
+
 		#if MODS_ALLOWED
-		if (FileSystem.exists(Paths.getPath('characters/$playBF-dead.json', TEXT, null, true)))
+		var hasCustomGameOver = FileSystem.exists(customCharPath);
 		#else
-		if (Assets.exists(Paths.getPath('characters/$playBF-dead.json', TEXT, null, true)))
+		var hasCustomGameOver = Assets.exists(customCharPath);
 		#end
-		{characterName = playBF + '-dead';}
+
+		if (hasCustomGameOver) {
+			characterName = playBF + '-dead';
+		} else {
+			characterName = 'bf-dead';
+		}
 
 		if(boyfriend == null)
 		{
-			boyfriend = new Character(PlayState.instance.boyfriend.getScreenPosition().x, PlayState.instance.boyfriend.getScreenPosition().y, characterName, true);
+			boyfriend = new Character(
+				PlayState.instance.boyfriend.getScreenPosition().x,
+				PlayState.instance.boyfriend.getScreenPosition().y,
+				characterName, true
+			);
 			boyfriend.x = PlayState.instance.boyfriend.x + offsetX;
 			boyfriend.y = PlayState.instance.boyfriend.y + offsetY;
 		}
@@ -111,55 +123,55 @@ class GameOverSubstate extends MusicBeatSubstate
 
 		FlxTween.tween(FlxG.camera, {zoom: 1}, 0.5, {ease: FlxEase.circInOut});
 		FlxTween.tween(bg, {alpha: 0.5}, 0.4, {ease: FlxEase.linear});
-		
-		PlayState.instance.setOnScripts('inGameOver', true);
-		PlayState.instance.callOnScripts('onGameOverStart', []);
-		FlxG.sound.music.loadEmbedded(Paths.music(loopSoundName), true);
+			
+			PlayState.instance.setOnScripts('inGameOver', true);
+			PlayState.instance.callOnScripts('onGameOverStart', []);
+			FlxG.sound.music.loadEmbedded(Paths.music(loopSoundName), true);
 
-		if(characterName == 'pico-dead')
-		{
-			overlay = new FlxSprite(boyfriend.x + 205, boyfriend.y - 80);
-			overlay.frames = Paths.getSparrowAtlas('Pico_Death_Retry');
-			overlay.animation.addByPrefix('deathLoop', 'Retry Text Loop', 24, true);
-			overlay.animation.addByPrefix('deathConfirm', 'Retry Text Confirm', 24, false);
-			overlay.antialiasing = ClientPrefs.data.antialiasing;
-			overlayConfirmOffsets.set(250, 200);
-			overlay.visible = false;
-			add(overlay);
-
-			boyfriend.animation.callback = function(name:String, frameNumber:Int, frameIndex:Int)
+			if(characterName == 'pico-dead')
 			{
-				switch(name)
+				overlay = new FlxSprite(boyfriend.x + 205, boyfriend.y - 80);
+				overlay.frames = Paths.getSparrowAtlas('Pico_Death_Retry');
+				overlay.animation.addByPrefix('deathLoop', 'Retry Text Loop', 24, true);
+				overlay.animation.addByPrefix('deathConfirm', 'Retry Text Confirm', 24, false);
+				overlay.antialiasing = ClientPrefs.data.antialiasing;
+				overlayConfirmOffsets.set(250, 200);
+				overlay.visible = false;
+				add(overlay);
+
+				boyfriend.animation.callback = function(name:String, frameNumber:Int, frameIndex:Int)
 				{
-					case 'firstDeath':
-						if(frameNumber >= 36 - 1)
-						{
-							overlay.visible = true;
-							overlay.animation.play('deathLoop');
+					switch(name)
+					{
+						case 'firstDeath':
+							if(frameNumber >= 36 - 1)
+							{
+								overlay.visible = true;
+								overlay.animation.play('deathLoop');
+								boyfriend.animation.callback = null;
+							}
+						default:
 							boyfriend.animation.callback = null;
-						}
-					default:
-						boyfriend.animation.callback = null;
+					}
 				}
-			}
 
-			if(PlayState.instance.gf != null && PlayState.instance.gf.curCharacter == 'nene')
-			{
-				var neneKnife:FlxSprite = new FlxSprite(boyfriend.x - 450, boyfriend.y - 250);
-				neneKnife.frames = Paths.getSparrowAtlas('NeneKnifeToss');
-				neneKnife.animation.addByPrefix('anim', 'knife toss', 24, false);
-				neneKnife.antialiasing = ClientPrefs.data.antialiasing;
-				neneKnife.animation.finishCallback = function(_)
+				if(PlayState.instance.gf != null && PlayState.instance.gf.curCharacter == 'nene')
 				{
-					remove(neneKnife);
-					neneKnife.destroy();
+					var neneKnife:FlxSprite = new FlxSprite(boyfriend.x - 450, boyfriend.y - 250);
+					neneKnife.frames = Paths.getSparrowAtlas('NeneKnifeToss');
+					neneKnife.animation.addByPrefix('anim', 'knife toss', 24, false);
+					neneKnife.antialiasing = ClientPrefs.data.antialiasing;
+					neneKnife.animation.finishCallback = function(_)
+					{
+						remove(neneKnife);
+						neneKnife.destroy();
+					}
+					insert(0, neneKnife);
+					neneKnife.animation.play('anim', true);
 				}
-				insert(0, neneKnife);
-				neneKnife.animation.play('anim', true);
 			}
-		}
 
-		super.create();
+			super.create();
 	}
 
 	override function update(elapsed:Float)
